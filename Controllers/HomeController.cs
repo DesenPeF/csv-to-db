27d378aefbab15e065c9b88e0247f89628e7e1c3
Isaias <isaiascxs10@gmail.com>
2022-09-13 18:50:11 -0300

    inicial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System.Data.SqlClient;
+using System.Diagnostics;
+using csv_to_db.Models;
+using Microsoft.AspNetCore.Mvc;
+using Dapper;
+
+
+namespace csv_to_db.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ConnectDb(ConnectionStringView cnnString)
+        {
+            SqlConnectionStringBuilder connectionString = new SqlConnectionStringBuilder();
+
+            connectionString.DataSource = cnnString.Server;
+            connectionString.UserID = cnnString.User;
+            connectionString.Password = cnnString.Password;
+            connectionString.InitialCatalog = cnnString.Database;
+
+            try
+            {
+                var cnn = new SqlConnection(connectionString.ConnectionString);
+                cnn.Open();
+                var eventos = cnn.Query("select * from Evento");
+                return View(eventos);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ConnectionStringView.cs
@@ -0,0 +1,13 @@
+﻿namespace csv_to_db.Models
+{
+    public class ConnectionStringView
+    {
+
+        public string Server { get; set; }
+        public string Database { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
